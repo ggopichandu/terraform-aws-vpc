995c1e1ccce2f342b6b6a7ee67218b390cf6d1fd
ggopichandu <gummagopichand@gmail.com>
2025-07-19 11:12:19 +0530

    vpc

--- vpc.tf
+++ vpc.tf
@@ -75,7 +75,7 @@ resource "aws_subnet" "database_subnet" {
 
 resource "aws_db_subnet_group" "default" {
 name = "${local.resource_name}"  
-subnet_ids = aws_subnet.database[*].id
+subnet_ids = aws_subnet.database_subnet[*].id
 
 tags = merge(
   var.common_tags,
