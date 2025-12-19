cb499052aae79a110821640805af2075be2393d1
Gopichand <gummagopichand@gmail.com>
2025-12-19 06:46:33 +0530

    aws-vpc

--- outputs.tf
+++ outputs.tf
@@ -7,15 +7,15 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-    value = aws_subnet.public_subnet[*].id  
+    value = aws_subnet.public[*].id  
 }
 
 output "private_subnet_ids" {
-    value = aws_subnet.private_subnet[*].id  
+    value = aws_subnet.private[*].id  
 }
 
 output "database_subnet_ids" {
-    value = aws_subnet.database_subnet[*].id 
+    value = aws_subnet.database[*].id 
 }
 
 output "database_subnet_group_id" {
@@ -27,6 +27,6 @@ output "database_subnet_group_name" {
 }
 
 output "igw_id" {
-    value = aws_internet_gateway.gw
+    value = aws_internet_gateway.gw.id
 }
 
--- vpc.tf
+++ vpc.tf
@@ -93,7 +93,7 @@ resource "aws_eip" "nat" {
 ### Nat Gateway ###
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.public_subnet[0].id
+  subnet_id = aws_subnet.public[0].id
   
 
   tags = merge(
@@ -170,19 +170,19 @@ resource "aws_route" "database_route_nat" {
 ### Route table and subnet associations ###
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
-  subnet_id      = element(aws_subnet.public_subnet[*].id, count.index)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidrs)
-  subnet_id      = element(aws_subnet.private_subnet[*].id, count.index)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
   count = length(var.database_subnet_cidrs)
-  subnet_id      = element(aws_subnet.database_subnet[*].id, count.index)
+  subnet_id      = element(aws_subnet.database[*].id, count.index)
   route_table_id = aws_route_table.database.id
 }
 
